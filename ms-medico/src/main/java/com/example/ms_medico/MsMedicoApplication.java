64fc2bab9cacca5f7c26803d5791dbd1a90b61fe
Marcilio Silva <marciliokaua12@gmail.com>
2025-10-08 10:58:48 -0300

    feat: atualizando local pacote

---
+++ ms-medico/src/main/java/com/example/ms_medico/MsMedicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_medico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMedicoApplication.class, args);
+	}
+
+}
