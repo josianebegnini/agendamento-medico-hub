661457f244e414313cfe2b51e1181eec985c0381
Caio Victor <caiopvasconcelos@gmail.com>
2025-10-08 11:13:33 -0300

    feat: implementando frontend

---
+++ agendamento-medico-front/src/main/java/com/agendamento/medico/agendamento_medico_front/AgendamentoMedicoFrontApplication.java
@@ -0,0 +1,13 @@
+package com.agendamento.medico.agendamento_medico_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoMedicoFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoMedicoFrontApplication.class, args);
+	}
+
+}
