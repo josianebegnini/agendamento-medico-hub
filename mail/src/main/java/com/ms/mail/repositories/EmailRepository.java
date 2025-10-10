e8b7813abb85a8c1d94355ffdc09460b3d1023ad
Marcilio Silva <marciliokaua12@gmail.com>
2025-10-10 00:03:17 -0300

    feat: configurando servico email

--- mail/src/main/java/com/ms/mail/repositories/EmailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ms.mail.repositories;
-
-import com.ms.mail.models.Email;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailRepository extends JpaRepository<Email, Long> {
-}
