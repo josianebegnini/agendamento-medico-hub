424e050aa779c2da210a9685a5f89fb6c68f14ab
Josiane Begnini <josianebegnini@gmail.com>
2025-10-15 13:13:59 -0300

    rabbit funcionando com docker

--- ms-agenda/src/main/java/com/example/msagenda/config/RabbitMQConfig.java
+++ ms-agenda/src/main/java/com/example/msagenda/config/RabbitMQConfig.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -20,30 +21,41 @@ public class RabbitMQConfig {
     public static final String QUEUE_NAME = "agendamento.medico.queue";
     public static final String ROUTING_KEY = "agendamento.medico.key";
 
+    // Exchange
     @Bean
     public DirectExchange exchange() {
         return new DirectExchange(EXCHANGE_NAME);
     }
 
+    // Queue
     @Bean
     public Queue queue() {
         return new Queue(QUEUE_NAME, true);
     }
 
+    // Binding
     @Bean
     public Binding binding(Queue queue, DirectExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
 
+    // Conversor JSON
     @Bean
     public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
+    // RabbitTemplate com conversor
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(jsonMessageConverter());
         return template;
+    }
+
+    // 🔹 RabbitAdmin: garante criação de exchange, queue e binding no startup
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
     }
 }
